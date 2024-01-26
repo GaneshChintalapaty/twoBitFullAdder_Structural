fb7e052936cca35e00fef7ad3d05c0f8fc4a6579
GaneshChintalapaty <ganesh.chintalapaty@gmail.com>
2024-01-26 12:04:10 -0600

    26012024_1204

--- fullAdder2Bit_Structural.runs/synth_1/fullAdder_2Bit.tcl
+++ fullAdder2Bit_Structural.runs/synth_1/fullAdder_2Bit.tcl
@@ -70,10 +70,7 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param checkpoint.writeSynthRtdsInDcp 1
 set_param chipscope.maxJobs 2
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7k70tfbv676-1
 
@@ -99,6 +96,8 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental C:/VivadoProjects/fullAdder2Bit_Structural/fullAdder2Bit_Structural.srcs/utils_1/imports/synth_1/fullAdder_2Bit.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
